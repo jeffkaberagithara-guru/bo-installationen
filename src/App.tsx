184bf36dc8b666a385fb17ce8a34cec4ae6a5ef9
jeffkaberagithara-guru <jeffkaberagithara@gmail.com>
2026-07-18 22:38:21 +0300

    Modified App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Hero from "./components/sections/Hero";
 import TrustBar from "./components/sections/TrustBar";
 import Services from "./components/sections/Services";
+import Innovation from "./components/sections/Innovation";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Hero />
       <TrustBar />
       <Services />
+      <Innovation />
     </main>
   );
 }
